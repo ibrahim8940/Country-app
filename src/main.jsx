50be985989477c6f205cb876fb0a75bb64b7d0a5
Ibrahim <ibraheem8940@gmail.com>
2025-12-27 15:56:48 +0500

    this is the initiol commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>
+  </HashRouter>
 );
